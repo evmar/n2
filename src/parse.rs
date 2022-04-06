evmar/n2

commit 7a7d10f152448d044551dc8ad343c9020b9d4a46

drop lookup tables in lexer
The path one is the one that really matters for performance, and it's
simpler to just express it as a negation -- it generates better code,
and it makes it properly accept non-ASCII.

src/parse.rs
@@ -51,25 +51,7 @@ pub struct Parser<'text> {
     path_buf: String,
 }
 
-/// Baseline implementation of is_ident_char.
-#[cfg(test)]
-fn is_ident_char_baseline(c: u8) -> bool {
-    match c as char {
-        'a'..='z' | 'A'..='Z' | '0'..='9' | '_' | '-' | '.' => true,
-        _ => false,
-    }
-}
-
-/// Lookup table implementation of is_ident_char.  Produces same output as
-/// _baseline version.  256-entry table is encoded as 4 64-bit integers.
-/// See gen_lookup_table.py for how it was generated.
 fn is_ident_char(c: u8) -> bool {
-    let lookup: [u64; 4] = [0x3ff600000000000, 0x7fffffe87fffffe, 0x0, 0x0];
-    (lookup[(c >> 6) as usize] & ((1 as u64) << (c & 63))) != 0
-}
-
-#[cfg(test)]
-fn is_path_char_baseline(c: u8) -> bool {
     match c as char {
         'a'..='z' | 'A'..='Z' | '0'..='9' | '_' | '-' | '.' | '/' | ',' | '+' | '@' => true,
         _ => false,
@@ -77,8 +59,11 @@ fn is_path_char_baseline(c: u8) -> bool {
 }
 
 fn is_path_char(c: u8) -> bool {
-    let lookup: [u64; 4] = [0x3fff80000000000, 0x7fffffe87ffffff, 0x0, 0x0];
-    (lookup[(c >> 6) as usize] & ((1 as u64) << (c & 63))) != 0
+    // Basically any character is allowed in paths, but we want to parse e.g.
+    //   build foo: bar | baz
+    // such that the colon is not part of the 'foo' path and such that '|' is
+    // not read as a path.
+    !matches!(c as char, '\0' | ' ' | '\n' | ':' | '|' | '$')
 }
 
 pub trait Loader {
@@ -421,24 +406,5 @@ default a b$var c
         };
         assert_eq!(default, vec!["a", "b3", "c"]);
         println!("{:?}", default);
-    }
-
-    #[test]
-    fn lookup_tables_match_baseline() {
-        for i in (0 as u8)..=255 {
-            assert_eq!(
-                is_ident_char(i),
-                is_ident_char_baseline(i),
-                "mismatch at {}",
-                i
-            );
-
-            assert_eq!(
-                is_path_char(i),
-                is_path_char_baseline(i),
-                "mismatch at {}",
-                i
-            );
-        }
     }
 }

src/gen_lookup_table.py
@@ -1,21 +1,0 @@
-#!/usr/bin/env python3
-
-# See parse.rs: is_ident_char().
-
-ident_chars = ['az', 'AZ', '09', '_', '-', '.']
-path_chars = ['az', 'AZ', '09', '_', '-', '.', '/', ',', '+', '@']
-chars = path_chars
-
-tab = [0 for _ in range(256)]
-for span in path_chars:
-  if len(span) > 1:
-    for c in range(ord(span[0]), ord(span[1])+1):
-      tab[c] = 1
-  else:
-    tab[ord(span)] = 1
-
-for ofs in range(0, 256, 64):
-  bits = tab[ofs:ofs+64]
-  s = ''.join('1' if b else '0' for b in bits)[::-1]
-  num = int(s, 2)
-  print(hex(num))

commit 27e31fd56e514d6b33bbdee335846adc1bb047e4

fix identifier bounds
I broke this in 7a7d10f152448d044551dc8ad343c9020b9d4a46, where I accidentally
replaced the impl of is_ident_char with the impl of is_path_char.

src/parse.rs
@@ -49,7 +49,7 @@ pub struct Parser<'text> {
 }
 
 fn is_ident_char(c: u8) -> bool {
-    matches!(c as char, 'a'..='z' | 'A'..='Z' | '0'..='9' | '_' | '-' | '.' | '/' | ',' | '+' | '@')
+    matches!(c as char, 'a'..='z' | 'A'..='Z' | '0'..='9' | '_' | '-' | '.')
 }
 
 fn is_path_char(c: u8) -> bool {