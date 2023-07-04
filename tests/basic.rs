b2cbdb7931b3bbd8838b203025e5a5e6aeb7fe3e
Evan Martin <evan.martin@gmail.com>
2023-07-03 23:31:49 -0700

    add a test documenting the current missing file behavior
    Part of #69.

--- tests/basic.rs
+++ tests/basic.rs
@@ -211,3 +211,29 @@ fn explain() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn missing_intermediate() -> anyhow::Result<()> {
+    let space = TestSpace::new()?;
+    space.write(
+        "build.ninja",
+        &[
+            TOUCH_RULE,
+            "rule echo
+  command = echo $out
+",
+            "build mid: echo",      // never writes output
+            "build out: touch mid", // uses never-written output
+            "",
+        ]
+        .join("\n"),
+    )?;
+
+    let out = space.run(&mut n2_command(vec!["out"]))?;
+    assert_output_contains(&out, "input mid missing");
+
+    // TODO: we'll need to revisit this behavior to fix
+    // https://github.com/evmar/n2/issues/69
+
+    Ok(())
+}
