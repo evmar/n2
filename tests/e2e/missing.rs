eb0433be82dc59cfc56251144f9dca4f268aa74e
Evan Martin <evan.martin@gmail.com>
2023-07-05 17:46:43 -0700

    more missing input tests

--- tests/e2e/missing.rs
+++ tests/e2e/missing.rs
@@ -41,3 +41,21 @@ fn missing_generated() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+// Ensure we don't regress on
+// https://github.com/ninja-build/ninja/issues/1779
+// I can't remember the specific code CMake generates that relies on this;
+// I wonder if we can tighten the behavior at all.
+#[test]
+fn missing_phony_input() -> anyhow::Result<()> {
+    let space = TestSpace::new()?;
+    space.write(
+        "build.ninja",
+        &[TOUCH_RULE, "build out: phony || no_such_file", ""].join("\n"),
+    )?;
+
+    let out = space.run_expect(&mut n2_command(vec!["out"]))?;
+    assert_output_contains(&out, "no work to do");
+
+    Ok(())
+}
