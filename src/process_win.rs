20720db275c1e46ea820a78fadb81b89f358ad2b
Evan Martin <evan.martin@gmail.com>
2023-07-10 13:29:15 -0700

    win: use std::mem::zeroed for structs

--- src/process_win.rs
+++ src/process_win.rs
@@ -10,38 +10,6 @@ fn GetLastError() -> u32 {
     unsafe { winapi::um::errhandlingapi::GetLastError() }
 }
 
-fn zeroed_startupinfo() -> winapi::um::processthreadsapi::STARTUPINFOA {
-    winapi::um::processthreadsapi::STARTUPINFOA {
-        cb: 0,
-        lpReserved: std::ptr::null_mut(),
-        lpDesktop: std::ptr::null_mut(),
-        lpTitle: std::ptr::null_mut(),
-        dwX: 0,
-        dwY: 0,
-        dwXSize: 0,
-        dwYSize: 0,
-        dwXCountChars: 0,
-        dwYCountChars: 0,
-        dwFillAttribute: 0,
-        dwFlags: 0,
-        wShowWindow: 0,
-        cbReserved2: 0,
-        lpReserved2: std::ptr::null_mut(),
-        hStdInput: winapi::um::handleapi::INVALID_HANDLE_VALUE,
-        hStdOutput: winapi::um::handleapi::INVALID_HANDLE_VALUE,
-        hStdError: winapi::um::handleapi::INVALID_HANDLE_VALUE,
-    }
-}
-
-fn zeroed_process_information() -> winapi::um::processthreadsapi::PROCESS_INFORMATION {
-    winapi::um::processthreadsapi::PROCESS_INFORMATION {
-        hProcess: std::ptr::null_mut(),
-        hThread: std::ptr::null_mut(),
-        dwProcessId: 0,
-        dwThreadId: 0,
-    }
-}
-
 pub fn run_command(cmdline: &str, _output_cb: impl FnMut(&[u8])) -> anyhow::Result<Termination> {
     // Don't want to run `cmd /c` since that limits cmd line length to 8192 bytes.
     // std::process::Command can't take a string and pass it through to CreateProcess unchanged,
@@ -51,7 +19,7 @@ pub fn run_command(cmdline: &str, _output_cb: impl FnMut(&[u8])) -> anyhow::Resu
         // TODO: Set this to just 0 for console pool jobs.
         let process_flags = winapi::um::winbase::CREATE_NEW_PROCESS_GROUP;
 
-        let mut startup_info = zeroed_startupinfo();
+        let mut startup_info = std::mem::zeroed::<winapi::um::processthreadsapi::STARTUPINFOA>();
         startup_info.cb = std::mem::size_of::<winapi::um::processthreadsapi::STARTUPINFOA>() as u32;
         startup_info.dwFlags = winapi::um::winbase::STARTF_USESTDHANDLES;
         startup_info.hStdInput =
@@ -60,8 +28,8 @@ pub fn run_command(cmdline: &str, _output_cb: impl FnMut(&[u8])) -> anyhow::Resu
             winapi::um::processenv::GetStdHandle(winapi::um::winbase::STD_OUTPUT_HANDLE);
         startup_info.hStdError = startup_info.hStdOutput;
 
-        let mut process_info = zeroed_process_information();
-
+        let mut process_info =
+            std::mem::zeroed::<winapi::um::processthreadsapi::PROCESS_INFORMATION>();
         let mut mut_cmdline = cmdline.to_string() + "\0";
 
         if winapi::um::processthreadsapi::CreateProcessA(
