0b10c92610495ebb4133f770bcda47b8abc6c89e
Evan Martin <evan.martin@gmail.com>
2023-07-11 10:10:10 -0700

    win: consume process stdout via pipe
    Based on a patch from Nico Weber <thakis@chromium.org>.

--- src/process_win.rs
+++ src/process_win.rs
@@ -1,12 +1,16 @@
 //! Implements run_command on Windows using native Windows calls.
 //! See run_command comments for why.
 
+use crate::process::Termination;
+use std::ffi::c_void;
+use std::io::Read;
+use std::os::windows::io::{FromRawHandle, OwnedHandle};
+use std::os::windows::prelude::AsRawHandle;
 use windows_sys::Win32::{
     Foundation::*,
-    System::{Console::*, Threading::*},
+    Security::SECURITY_ATTRIBUTES,
+    System::{Console::*, Pipes::CreatePipe, Threading::*},
 };
-
-use crate::process::Termination;
 
 /// Wrapper for PROCESS_INFORMATION that cleans up on Drop.
 struct ProcessInformation(PROCESS_INFORMATION);
@@ -45,26 +49,120 @@ impl Drop for ProcessInformation {
     }
 }
 
+/// Wrapper for PROC_THREAD_ATTRIBUTE_LIST.  This is a type whose size we discover at runtime.
+struct ProcThreadAttributeList(Box<[u8]>);
+impl ProcThreadAttributeList {
+    fn new(count: usize) -> anyhow::Result<Self> {
+        unsafe {
+            let mut size = 0;
+            if InitializeProcThreadAttributeList(std::ptr::null_mut(), count as u32, 0, &mut size)
+                == 0
+            {
+                if GetLastError() != ERROR_INSUFFICIENT_BUFFER {
+                    anyhow::bail!(
+                        "{}: {}",
+                        "InitializeProcThreadAttributeList",
+                        GetLastError()
+                    );
+                }
+            }
+
+            let mut buf = vec![0u8; size].into_boxed_slice();
+            if InitializeProcThreadAttributeList(
+                buf.as_mut_ptr() as LPPROC_THREAD_ATTRIBUTE_LIST,
+                count as u32,
+                0,
+                &mut size,
+            ) == 0
+            {
+                anyhow::bail!(
+                    "{}: {}",
+                    "InitializeProcThreadAttributeList",
+                    GetLastError()
+                );
+            }
+            Ok(Self(buf))
+        }
+    }
+
+    fn inherit_handles(&mut self, handles: &[HANDLE]) -> anyhow::Result<()> {
+        unsafe {
+            if UpdateProcThreadAttribute(
+                self.as_mut_ptr(),
+                0,
+                PROC_THREAD_ATTRIBUTE_HANDLE_LIST as usize,
+                handles.as_ptr() as *const c_void,
+                handles.len() * std::mem::size_of::<HANDLE>(),
+                std::ptr::null_mut(),
+                std::ptr::null_mut(),
+            ) == 0
+            {
+                anyhow::bail!("{}: {}", "UpdateProcThreadAttribute", GetLastError());
+            }
+        }
+        Ok(())
+    }
+
+    fn as_mut_ptr(&mut self) -> LPPROC_THREAD_ATTRIBUTE_LIST {
+        self.0.as_mut_ptr() as LPPROC_THREAD_ATTRIBUTE_LIST
+    }
+}
+
+impl Drop for ProcThreadAttributeList {
+    fn drop(&mut self) {
+        unsafe { DeleteProcThreadAttributeList(self.as_mut_ptr()) };
+    }
+}
+
 #[allow(non_snake_case)]
 fn GetLastError() -> u32 {
     unsafe { windows_sys::Win32::Foundation::GetLastError() }
 }
 
-pub fn run_command(cmdline: &str, _output_cb: impl FnMut(&[u8])) -> anyhow::Result<Termination> {
+pub fn run_command(cmdline: &str, mut output_cb: impl FnMut(&[u8])) -> anyhow::Result<Termination> {
     // Don't want to run `cmd /c` since that limits cmd line length to 8192 bytes.
     // std::process::Command can't take a string and pass it through to CreateProcess unchanged,
     // so call that ourselves.
+    // https://github.com/rust-lang/rust/issues/38227
 
-    let exit_code = unsafe {
+    let (pipe_read, pipe_write) = unsafe {
+        let mut pipe_read: HANDLE = 0;
+        let mut pipe_write: HANDLE = 0;
+        let mut attrs = std::mem::zeroed::<SECURITY_ATTRIBUTES>();
+        attrs.nLength = std::mem::size_of::<SECURITY_ATTRIBUTES>() as u32;
+        attrs.bInheritHandle = TRUE;
+        if CreatePipe(
+            &mut pipe_read,
+            &mut pipe_write,
+            &mut attrs,
+            /* use default buffer size */ 0,
+        ) == 0
+        {
+            anyhow::bail!("{}: {}", "CreatePipe", GetLastError());
+        }
+        (
+            OwnedHandle::from_raw_handle(pipe_read as *mut c_void),
+            OwnedHandle::from_raw_handle(pipe_write as *mut c_void),
+        )
+    };
+
+    let process_info = unsafe {
         // TODO: Set this to just 0 for console pool jobs.
-        let process_flags = CREATE_NEW_PROCESS_GROUP;
+        let process_flags = CREATE_NEW_PROCESS_GROUP | EXTENDED_STARTUPINFO_PRESENT;
 
-        let mut startup_info = std::mem::zeroed::<STARTUPINFOA>();
-        startup_info.cb = std::mem::size_of::<STARTUPINFOA>() as u32;
-        startup_info.dwFlags = STARTF_USESTDHANDLES;
-        startup_info.hStdInput = GetStdHandle(STD_INPUT_HANDLE);
-        startup_info.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-        startup_info.hStdError = startup_info.hStdOutput;
+        let mut startup_info = std::mem::zeroed::<STARTUPINFOEXA>();
+        startup_info.StartupInfo.cb = std::mem::size_of::<STARTUPINFOEXA>() as u32;
+        startup_info.StartupInfo.dwFlags = STARTF_USESTDHANDLES;
+        startup_info.StartupInfo.hStdInput = GetStdHandle(STD_INPUT_HANDLE);
+        let raw_pipe_write = pipe_write.as_raw_handle() as isize;
+        startup_info.StartupInfo.hStdOutput = raw_pipe_write;
+        startup_info.StartupInfo.hStdError = raw_pipe_write;
+
+        // Safely inherit in/out handles.
+        // https://devblogs.microsoft.com/oldnewthing/20111216-00/?p=8873
+        let mut attrs = ProcThreadAttributeList::new(1)?;
+        attrs.inherit_handles(&[startup_info.StartupInfo.hStdInput, raw_pipe_write])?;
+        startup_info.lpAttributeList = attrs.as_mut_ptr();
 
         let mut process_info = ProcessInformation::new();
 
@@ -80,13 +178,28 @@ pub fn run_command(cmdline: &str, _output_cb: impl FnMut(&[u8])) -> anyhow::Resu
             process_flags,
             std::ptr::null_mut(),
             std::ptr::null_mut(),
-            &mut startup_info,
+            &mut startup_info.StartupInfo,
             process_info.as_mut_ptr(),
         ) == 0
         {
             anyhow::bail!("{}: {}", "CreateProcessA", GetLastError());
         }
+        drop(pipe_write);
 
+        process_info
+    };
+
+    let mut pipe = std::fs::File::from(pipe_read);
+    let mut buf: [u8; 4 << 10] = [0; 4 << 10];
+    loop {
+        let n = pipe.read(&mut buf)?;
+        if n == 0 {
+            break;
+        }
+        output_cb(&buf[0..n]);
+    }
+
+    let exit_code = unsafe {
         if WaitForSingleObject(process_info.hProcess, INFINITE) != 0 {
             anyhow::bail!("{}: {}", "WaitForSingleObject", GetLastError());
         }
--- tests/manual/prints/prints.bat
+++ tests/manual/prints/prints.bat
@@ -5,6 +5,3 @@ echo %1: 2
 timeout /T 1 /NOBREAK > nul
 echo %1: 3
 timeout /T 1 /NOBREAK > nul
-echo %1: 4
-timeout /T 1 /NOBREAK > nul
-echo %1: 5
