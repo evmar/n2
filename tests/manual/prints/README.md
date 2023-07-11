7722d045c4c6e8716415859117abe40fa449632d
Evan Martin <evan.martin@gmail.com>
2023-07-11 09:47:14 -0700

    manual test for interleaved prints

---
+++ tests/manual/prints/README.md
@@ -0,0 +1,2 @@
+This manual test runs multiple subcommands that write to stdout, to verify
+interleaving output.
---
+++ tests/manual/prints/prints.bat
@@ -0,0 +1,10 @@
+@echo off
+echo %1: 1
+timeout /T 1 /NOBREAK > nul
+echo %1: 2
+timeout /T 1 /NOBREAK > nul
+echo %1: 3
+timeout /T 1 /NOBREAK > nul
+echo %1: 4
+timeout /T 1 /NOBREAK > nul
+echo %1: 5
