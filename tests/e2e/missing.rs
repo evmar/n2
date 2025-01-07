3b53d99507212b10efddae17b4b9f5f3b1d5169c
Evan Martin <evan.martin@gmail.com>
2025-01-07 11:11:48 -0800

    add failing test for missing phony file

--- tests/e2e/missing.rs
+++ tests/e2e/missing.rs
@@ -81,7 +81,39 @@ fn missing_phony_input() -> anyhow::Result<()> {
 }
 
 #[test]
-fn phony_output_on_disk() -> anyhow::Result<()> {
+fn phony_missing_file() -> anyhow::Result<()> {
+    // https://ninja-build.org/manual.html#_the_literal_phony_literal_rule
+    //   build foo: phony
+    // means "don't fail the build if foo doesn't exist, even in inputs"
+
+    let space = TestSpace::new()?;
+    space.write(
+        "build.ninja",
+        &[
+            TOUCH_RULE,
+            "build out: touch | phony_file",
+            "build phony_file: phony",
+            "",
+        ]
+        .join("\n"),
+    )?;
+
+    // Expect the first build to generate some state...
+    let out = space.run_expect(&mut n2_command(vec!["out"]))?;
+    assert_output_contains(&out, "ran 1 task");
+    // ...but a second one should be up to date.
+    let out = space.run_expect(&mut n2_command(vec!["out"]))?;
+    // BUG: this should be
+    // assert_output_contains(&out, "no work to do");
+    // but it is currently
+    assert_output_contains(&out, "ran 1 task");
+
+    Ok(())
+}
+
+#[test]
+fn phony_existing_file() -> anyhow::Result<()> {
+    // Like phony_missing_file, but the file exists on disk.
     // https://github.com/evmar/n2/issues/40
     // CMake uses a phony rule targeted at a real file as a way of marking
     // "don't fail the build if this file is missing", but it had the consequence
